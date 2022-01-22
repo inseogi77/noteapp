853afc61934d2945811b4e15c751c71219e4e869
inseogi77 <inseogi77@gmail.com>
2022-01-22 11:19:58 +0900

    evernote project change
    evernote project change

---
+++ EvernoteClone/ViewModel/NotesVM.cs
@@ -0,0 +1,101 @@
+﻿using EvernoteClone.Model;
+using EvernoteClone.ViewModel.Commands;
+using EvernoteClone.ViewModel.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace EvernoteClone.ViewModel
+{
+    public class NotesVM : INotifyPropertyChanged
+    {
+        public ObservableCollection<Notebook> Notebooks { get; set; }
+        public ObservableCollection<Note> Notes { get; set; }
+        private Notebook selectedNotebook;
+        public Notebook SelectedNotebook
+        {
+            get { return selectedNotebook; }
+            set
+            {
+                selectedNotebook = value;
+                OnPropertyChanged("SelectedNotebook");
+                GetNotes();
+            }
+        }
+        public NewNotebookCommand NewNotebookCommand { get; set; }
+        public NewNoteCommand NewNoteCommand { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public NotesVM()
+        {
+            NewNoteCommand = new NewNoteCommand(this);
+            NewNotebookCommand = new NewNotebookCommand(this);
+
+            Notebooks = new ObservableCollection<Notebook>();
+            Notes = new ObservableCollection<Note>();
+
+            GetNotebooks();
+        }
+
+        public void CreateNotebook()
+        {
+            Notebook newNotebook = new Notebook
+            {
+                Name = "Notebook"
+            };
+
+            DatabaseHelper.Insert(newNotebook);
+
+            GetNotebooks();
+        }
+
+        public void CreateNote(int notebookId)
+        {
+            Note newNote = new Note
+            {
+                NotebookId = notebookId,
+                CreatedAt = DateTime.Now,
+                UpdatedAt = DateTime.Now,
+                Title = $"Note for {DateTime.Now.ToString()}"
+            };
+
+            DatabaseHelper.Insert(newNote);
+
+            GetNotes();
+        }
+
+        private void GetNotebooks()
+        {
+            var notebooks = DatabaseHelper.Read<Notebook>();
+
+            Notebooks.Clear();
+            foreach(var notebook in notebooks)
+            {
+                Notebooks.Add(notebook);
+            }
+        }
+
+        private void GetNotes()
+        {
+            if (SelectedNotebook != null)
+            {
+                var notes = DatabaseHelper.Read<Note>().Where(n => n.NotebookId == SelectedNotebook.Id).ToList();
+
+                Notes.Clear();
+                foreach (var note in notes)
+                {
+                    Notes.Add(note);
+                }
+            }
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
